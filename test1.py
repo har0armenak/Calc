ada54c9ab9a1ad23cd162dac9818827f1639ab5d
LilithSed <lilithsedrakyan98@gmail.com>
2022-02-03 17:43:15 +0400

    Adding Functions

--- test1.py
+++ test1.py
@@ -1,13 +1,28 @@
 a = int(input("please input 1st number"))
 b = int(input("please input 2nd number"))
-x = input("please input operator sign for math action")
+x = input("please input the operator sign")
+
+
+def sum(a, b):
+    return a + b
+
+
+def division(a, b):
+    return a/b
+
+
+def multiplication(a, b):
+    return a*b
+
+
+def minus(a, b):
+    return a - b
 
 if (x == '+'):
-    print(a+b)
+    print(sum(a, b))
 elif (x == '-'):
-    print(a-b)
+    print(minus(a, b))
 elif (x == '*'):
-    print(a*b)
+    print(multiplication(a, b))
 elif (x == '/'):
-    print(a/b)
-
+    print(division(a, b))
