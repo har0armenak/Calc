e401cccf5d32ff02689bef9cca03efc9f0266e6f
har0armenak <har0armenak@gmail.com>
2022-02-03 17:20:07 +0400

    inital programm

---
+++ test1.py
@@ -0,0 +1,13 @@
+a = int(input("please input 1st number"))
+b = int(input("please input 2nd number"))
+x = input("please input operator sign for math action")
+
+if (x == '+'):
+    print(a+b)
+elif (x == '-'):
+    print(a-b)
+elif (x == '*'):
+    print(a*b)
+elif (x == '/'):
+    print(a/b)
+
